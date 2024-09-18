5b1b17766fc842667c3d0913e2076157cd854c50
bereket-desta <bereketdesta51@gmail.com>
2024-09-18 18:49:54 +0100

    Updates

---
+++ precache-manifest.402077bb4ccacc3cc4b835d695ecf941.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50eec300d6e9c714d124b6bdb5fa51e",
+    "url": "/React-newYear_project/index.html"
+  },
+  {
+    "revision": "92a5832f31432a241bdc",
+    "url": "/React-newYear_project/static/css/main.07a86098.chunk.css"
+  },
+  {
+    "revision": "0998ae281dc6077d040d",
+    "url": "/React-newYear_project/static/js/2.c908b332.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/React-newYear_project/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "92a5832f31432a241bdc",
+    "url": "/React-newYear_project/static/js/main.48ba71c0.chunk.js"
+  },
+  {
+    "revision": "f3df6d6f8fb19c309ba8",
+    "url": "/React-newYear_project/static/js/runtime~main.11c7a850.js"
+  },
+  {
+    "revision": "44e237e65a08aa73d91d04f10a0e598e",
+    "url": "/React-newYear_project/static/media/num-0.44e237e6.jpg"
+  },
+  {
+    "revision": "db7bccacb569f871b5e038b817cb6189",
+    "url": "/React-newYear_project/static/media/num-1.db7bccac.jpg"
+  },
+  {
+    "revision": "9c63e3596b09d6d431d1176b6f4fd23e",
+    "url": "/React-newYear_project/static/media/num-2.9c63e359.jpg"
+  },
+  {
+    "revision": "db619cbb8b2a8ccb6ff205c4cbca8636",
+    "url": "/React-newYear_project/static/media/num-7.db619cbb.jpg"
+  }
+]);
